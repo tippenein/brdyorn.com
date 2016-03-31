10410f388812127dd5df03a90d4b19c499116626
brady.ouren <brady.ouren@gmail.com>
2016-03-30 23:13:53 -0400

    ASDFasdfASFDfdsASDF

--- posts/2013-03-20-call-construct-in-racket.md
+++ posts/2013-03-20-call-construct-in-racket.md
@@ -6,20 +6,24 @@ title: Structure of 'call' in a programming language
 ### Grokking the 'call' construct
 While implementing the MUPL (Made Up Programming Language) for a programming languages course I found call to be interesting. I'll underline some of the implementation here.
 
-Now, call isn't necessarily a difficult concept. It's used all the time in jQuery and javascript to alter the `this` of a function call and here we'll do a similar thing, but it seems more fun because you're also using lexical scope and closure environment within the call implementation. It's easier if I just show some of the code.
+Now, call isn't necessarily a difficult concept. It's used all the time in
+jQuery and javascript to alter the `this` of a function call and here we'll do
+a similar thing, but it seems more fun because you're also using lexical scope
+and closure environment within the call implementation. It's easier if I just
+show some of the code.
 
 <!--more-->
 
 ``` scheme
-    (define (eval-exp e)
-        (eval-under-env e '())) ;; start with an empty list environment
+(define (eval-exp e)
+    (eval-under-env e '())) ;; start with an empty list environment
 ```
 
 This is what's called from within our language. Each expression has an environment which in this case is just a list of pairs of expressions. Briefly, when you create a struct you get helper function for checking an expression and the equivalent of getters. ex. `(struct add (e1 e2))` will allow you to test any expression with `(add? e)` and get `(+ add-e1 add-e2)` etc. These are the structs we care about here:
 
 ``` scheme
-    (struct call (funexp actual) #:transparent)
-    (struct closure (env fun) #:transparent)
+(struct call (funexp actual) #:transparent)
+(struct closure (env fun) #:transparent)
 ```
 
 ### eval within a specific environment
@@ -28,22 +32,22 @@ The fun comes with defining `eval-under-env`. This is the given spec for impleme
 > A call evaluates its first and second subexpressions to values. If the first is not a closure, it is an error. Else, it evaluates the closure's function's body in the closure's environment __extended to map the function's name to the closure__ (unless the name field is #f) and the functions argument to the result of the second subexpression.
 
 ``` scheme
-    (define (eval-under-env e env)
-        (cond   [(var? e) (envlookup env (var-string e))]
-                ;; ...more expression checking
-                [(call? e)
+(define (eval-under-env e env)
+    (cond   [(var? e) (envlookup env (var-string e))]
+            ;; ...more expression checking
+            [(call? e)
 ```
 
 eval-under-env is a large `cond` checking the given expression (`e`) within the given environment (`env`). The following is how we evaluate `call` expressions:
 
 ``` scheme
-    (let ([v1 (eval-under-env (call-funexp e) env)]
-          [v2 (eval-under-env (call-actual e) env)])
-        (if (closure? v1) ;; make sure first expression is a closure
-            (let* [carg1 (closure-fun v1)]
-                  [carg2 (closure-env v1)]
-                  [cname (cons (fun-nameopt carg1) v1)]
-                  [cfunc (cons (fun-formal carg2) v2)])
+(let ([v1 (eval-under-env (call-funexp e) env)]
+      [v2 (eval-under-env (call-actual e) env)])
+    (if (closure? v1) ;; make sure first expression is a closure
+        (let* [carg1 (closure-fun v1)]
+              [carg2 (closure-env v1)]
+              [cname (cons (fun-nameopt carg1) v1)]
+              [cfunc (cons (fun-formal carg2) v2)])
 ```
 
 The `let*` is basically allowing contiguous use of bindings within the same let binding (ex. `carg1` is used within the `cname` binding). After the args are set to cname and cfunc respectively, the function is evaluated within the specified closure-env (`carg2`) but we have to construct that environment.
--- site.hs
+++ site.hs
@@ -4,85 +4,99 @@ import Data.List
 import Data.Monoid
 import Hakyll
 
+contentPage :: String -> Rules ()
+contentPage title = do
+  route idRoute
+  compile $ do
+    let indexCtx = constField "title" title <> defaultContext
+
+    getResourceBody
+      >>= applyAsTemplate indexCtx
+      >>= loadAndApplyTemplate "templates/default.html" indexCtx
+      >>= relativizeUrls
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "keybase.txt" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+  match (fromList ["about.rst", "contact.markdown"]) $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+              listField "posts" postCtx (return posts)
+           <> constField "title" "Archives"
+           <> defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- loadAll "posts/*" >>= fmap (take 10) . recentFirst
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- loadAll "posts/*" >>= fmap (take 10) . recentFirst
+      let indexCtx =
+              listField "posts" postCtx (return posts)
+           <> constField "title" "Home"
+           <> defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
+  match "templates/*" $ compile templateCompiler
 
-    -- Render RSS feed
-    create ["rss.xml"] $ do
-        route idRoute
-        compile $ do
-            loadAllSnapshots "posts/*" "content"
-                >>= fmap (take 10) . recentFirst
-                >>= renderRss feedConfiguration feedCtx
+  -- Render RSS feed
+  create ["rss.xml"] $ do
+    route idRoute
+    compile $
+      loadAllSnapshots "posts/*" "content"
+        >>= fmap (take 10) . recentFirst
+        >>= renderRss feedConfiguration feedCtx
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" <> defaultContext
 
 feedCtx :: Context String
 feedCtx = mconcat
-    [ bodyField "description"
-    , defaultContext
-    ]
+  [ bodyField "description"
+  , defaultContext
+  ]
 
 feedConfiguration :: FeedConfiguration
 feedConfiguration = FeedConfiguration
-    { feedTitle       = "Brady Ouren's home page feed."
-    , feedDescription = "Brady Ouren's home page feed."
-    , feedAuthorName  = "Brady Ouren"
-    , feedAuthorEmail = "brdyorn@andand.co"
-    , feedRoot        = "http://brdyorn.com"
-    }
+  { feedTitle       = "Brady Ouren's home page feed."
+  , feedDescription = "Brady Ouren's home page feed."
+  , feedAuthorName  = "Brady Ouren"
+  , feedAuthorEmail = "brdyorn@andand.co"
+  , feedRoot        = "http://brdyorn.com"
+  }
