e9a614531409b1a508aa63f95c4856fc5567f56e
brady.ouren <brady.ouren@gmail.com>
2016-04-01 16:54:07 -0700

    slides, keybase, default templating changes

--- site.hs
+++ site.hs
@@ -22,6 +22,10 @@ main = hakyll $ do
     route   idRoute
     compile copyFileCompiler
 
+  match "slides/*" $ do
+    route   idRoute
+    compile copyFileCompiler
+
   match "images/*" $ do
     route   idRoute
     compile copyFileCompiler
